0ed2483695f358e904f303e407acacd539df5659
xxXWarMachineRoXxx <rijul123.sharma@outlook.com>
2023-04-24 19:51:14 +0530

    tried adding user name to chat conversations top level but it aint ahpepning

--- src/components/chatBody/chatBody.tsx
+++ src/components/chatBody/chatBody.tsx
@@ -7,8 +7,7 @@ interface ChatBodyProps {
 }
 
 function ChatBody({url }: ChatBodyProps) {
-  const [cardData, setData] = useState<any>({ all_count: 0 });
-  const [prevAllCount, setPrevAllCount] = useState<number | null>(null);
+  const [cardData, setData] = useState<any>([]);
   const [cards, setCards] = useState<Array<JSX.Element>>([]);
 
   useEffect(() => {
@@ -16,29 +15,32 @@ function ChatBody({url }: ChatBodyProps) {
       try {
         const response = await fetch(url);
         const json = await response.json();
-        if (prevAllCount !== null && json.all_count !== cardData.all_count) {
+        
           // If all_count has changed, update the previous value and make the API call
-          setData(json);
-          setPrevAllCount(json.all_count);
-        } else {
-          // Otherwise, only update the previous value
-          setPrevAllCount(json.all_count);
-        }
+          setData(json.data);
+        
+       
       } catch (error) {
         console.error(error);
       }
     };
-    makeAPICall();
-  }, [cardData.all_count, prevAllCount, url]);
-
+    makeAPICall().then((response) => {
+      console.log('done');
+      console.log(cardData);
+      console.log(response)
+    })
+    
+  }, []);
+  
+  
   useEffect(() => {
     const newCards = [];
-    for (let i = 0; i < cardData.all_count; i++) {
-      const cardProps: ChatCardProps = { key: i,text: `Oh no! ${i}`, userName: `Robert ${i + 1}`,chatId: `${i}` };
+    for (let i = 0; i < cardData.meta.all_count; i++) {
+      const cardProps: ChatCardProps = { key: i,text: cardData.payload.messages.content, userName: cardData.payload.meta.name,chatId: `${i}` };
       newCards.push(<ChatCard {...cardProps} />);
     }
     setCards(newCards);
-  }, [cardData.all_count]);
+  }, []);
 
   return (
     <div>
